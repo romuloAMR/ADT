e20a30e7515d708603998a1c02c96e7665900125
romuloAMR <amr.rmulo@gmail.com>
2024-08-14 16:20:44 -0300

    Nodes init

---
+++ Node.hpp
@@ -0,0 +1,61 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+template <typename T>
+class Node
+{
+private:
+    T data;
+    Node *next;
+
+public:
+    // Constructors and Destructor
+    Node(T data, Node *next = nullptr);
+    ~Node();
+
+    // Getters and Setters
+    T getData();
+    void setData(T data);
+
+    Node *getNext();
+    void setNext(Node *next);
+};
+
+#endif
+
+// Constructors and Destructor
+template <typename T>
+Node<T>::Node(T data, Node *next) : data(data), next(next)
+{
+}
+
+template <typename T>
+Node<T>::~Node()
+{
+    // Nothing
+}
+
+// Getters and Setters
+template <typename T>
+T Node<T>::getData()
+{
+    return data;
+}
+
+template <typename T>
+void Node<T>::setData(T data)
+{
+    this->data = data;
+}
+
+template <typename T>
+Node<T> *Node<T>::getNext()
+{
+    return next;
+}
+
+template <typename T>
+void Node<T>::setNext(Node *next)
+{
+    this->next = next;
+}
---
+++ Node2.hpp
@@ -0,0 +1,82 @@
+#ifndef NODE2_HPP
+#define NODE2_HPP
+
+template <typename T>
+class Node2
+{
+private:
+    T data;
+    Node2 *next;
+    Node2 *before;
+
+public:
+    // Constructors and Destructor
+    Node2(T data, Node2 *before = nullptr, Node2 *next = nullptr);
+    ~Node2();
+
+    // Getters and Setters
+    T getData();
+    void setData(T data);
+
+    Node2 *getNext();
+    void setNext(Node2 *next);
+
+    Node2 *getBefore();
+    void setBefore(Node2 *before);
+};
+
+#endif
+
+// Constructors and Destructor
+template <typename T>
+Node2<T>::Node2(T data, Node2 *before, Node2 *next) : data(data), before(before), next(next)
+{
+}
+
+template <typename T>
+Node2<T>::~Node2()
+{
+    if (before != nullptr) {
+        before->setNext(next);
+    }
+    if (next != nullptr) {
+        next->setBefore(before);
+    }
+}
+
+// Getters and Setters
+template <typename T>
+T Node2<T>::getData()
+{
+    return data;
+}
+
+template <typename T>
+void Node2<T>::setData(T data)
+{
+    this->data = data;
+}
+
+template <typename T>
+Node2<T> *Node2<T>::getNext()
+{
+    return next;
+}
+
+template <typename T>
+void Node2<T>::setNext(Node2 *next)
+{
+    this->next = next;
+}
+
+template <typename T>
+Node2<T> *Node2<T>::getBefore()
+{
+    return before;
+}
+
+template <typename T>
+void Node2<T>::setBefore(Node2 *before)
+{
+    this->before = before;
+}
