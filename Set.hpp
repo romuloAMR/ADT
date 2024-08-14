a13647a86900450fd66b623e06ef47dcf8f8cf35
romuloAMR <amr.rmulo@gmail.com>
2024-08-14 12:41:37 -0300

    Set init

---
+++ Set.hpp
@@ -0,0 +1,73 @@
+#ifndef SET_HPP
+#define SET_HPP
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+template <typename T>
+class Set
+{
+private:
+    std::vector<T> set;
+public:
+    Set();
+    ~Set();
+    void add(const T& element);
+    void pop(const T& element);
+    bool contains(const T& element) const;
+    int len() const;
+
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& os, const Set<U>& s);
+};
+
+template <typename T>
+Set<T>::Set() = default;
+
+template <typename T>
+Set<T>::~Set() = default;
+
+template <typename T>
+void Set<T>::add(const T& element)
+{
+    if (!contains(element))
+    {
+        set.push_back(element);
+    }
+}
+
+template <typename T>
+void Set<T>::pop(const T& element)
+{
+    auto it = std::find(set.begin(), set.end(), element);
+    if (it != set.end())
+    {
+        set.erase(it);
+    }
+}
+
+template <typename T>
+bool Set<T>::contains(const T& element) const
+{
+    return std::find(set.begin(), set.end(), element) != set.end();
+}
+
+template <typename T>
+int Set<T>::len() const
+{
+    return set.size();
+}
+
+template <typename U>
+std::ostream &operator<<(std::ostream &os, const Set<U> &s)
+{
+    os << "{ ";
+    for (const auto& elem : s.set) {
+        os << elem << " ";
+    }
+    os << "}";
+    return os;
+}
+
+#endif
